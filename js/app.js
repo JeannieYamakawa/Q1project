04935a9dc850bb4c7d0cc9b099190a0aa20d6cd6
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-09-06 18:46:28 -0500

    changes to JS

--- js/app.js
+++ js/app.js
@@ -1,5 +1,46 @@
 $( function() {
-    console.log( "ready!" );
+    var $body = $( ".pg1body" );
+    var $userInput = $( "#food-table" );
+    var $form = $( "#theForm" );
+    var $cuisineType;
+    var $userZipCode = $( "#inputText" );
+    console.log( $userZipCode )
+    var $userInput;
+    var $within1Mile;
+    var $within2Miles;
+    var $within5Miles;
+    var $within10Miles;
+    var $checkedZipCode;
+    var $arrayOfCuisine;
+    var $button = $( '.button' )
+    console.log( $button )
+    var $truck = $( '.truck' )
+
+
+
+    $( $body ).click( function( event ) {
+        event.preventDefault()
+        var cuisineType = event.target.title
+        cuisineType = cuisineType.toUpperCase();
+        console.log( cuisineType ) //
+        window.location.href = 'file:///Users/admin/galvanize/Q1project/page2.html';
+        return false;
+    } );
+
+    $userZipCode.click( function( event ) {
+        event.preventDefault()
+    } )
+    $button.click( function( event ) {
+        event.preventDefault()
+        var zipCodeToCheck = $userZipCode.val()
+
+
+
+
+
+
+
+    } );
 
 
 
@@ -9,4 +50,4 @@ $( function() {
 
 
 
-} );
+} )
